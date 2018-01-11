6cb4210c25ce2240462dcef7eb9fb8802e5286cf
Arvis Abromovics <info@vsmedio.lv>
2018-01-11 20:41:39 +0100

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,46 @@
+/* 
+Шифровка
+Придумать механизм шифровки/дешифровки.
+
+Программа запускается с одним из следующих наборов параметров:
+-e fileName fileOutputName
+-d fileName fileOutputName
+
+где:
+fileName - имя файла, который необходимо зашифровать/расшифровать.
+fileOutputName - имя файла, куда необходимо записать результат шифрования/дешифрования.
+-e - ключ указывает, что необходимо зашифровать данные.
+-d - ключ указывает, что необходимо расшифровать данные.
+
+
+Требования:
+1. Считывать с консоли ничего не нужно.
+2. Создай поток для чтения из файла, который приходит вторым параметром ([fileName]).
+3. Создай поток для записи в файл, который приходит третьим параметром ([fileOutputName]).
+4. В режиме "-e" программа должна зашифровать [fileName] и записать в [fileOutputName].
+5. В режиме "-d" программа должна расшифровать [fileName] и записать в [fileOutputName].
+6. Созданные для файлов потоки должны быть закрыты.
+*/
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        FileInputStream fin = new FileInputStream(args[1]);
+        FileOutputStream fout = new FileOutputStream(args[2]);
+
+        while (fin.available() > 0) {
+            int b = fin.read();
+            if (args[0].equals("-e"))  // encryption
+                b = b + 1;
+            if (args[0].equals("-d"))  // decryption
+                b = b - 1;
+            fout.write(b);
+        }
+
+        fin.close();
+        fout.close();
+    }
+}
